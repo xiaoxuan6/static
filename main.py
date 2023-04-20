679e4bd5b191f5b4d198d45a2d33e7bcc933982d
xiaoxuan6 <1527736751@qq.com>
2023-04-20 14:58:13 +0800

    fix: 根据url保存图片到本地

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-import base64
+import argparse
 import datetime
 import os
-import sys
+
+import requests
 
 
-def main(data: str, filename: str):
+def main(url: str, filename: str):
     date = datetime.date.today()
     path = r"{}".format(date).replace('-', '/')
     if not os.path.exists(path):
         os.makedirs(path)
 
-    img = base64.b64decode(data)
-    filepath = r"{}/{}/{}".format(os.getcwd(), path, filename)
-    with open(filepath, mode='wb') as f:
-        f.write(img)
+    response = requests.get(url)
+    if response.status_code == 200:
+        filepath = r"{}/{}/{}".format(os.getcwd(), path, filename)
+        with open(filepath, mode='wb') as f:
+            f.write(response.content)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        item = list(sys.argv)
-        main(item[1], item[2])
-    else:
-        print("invalid params")
+    parse = argparse.ArgumentParser()
+    parse.add_argument('--url', type=str, default='https://transfer.sh/QSJlve/2023-03-17212303.png')
+    parse.add_argument('--filename', type=str, default='a.png')
+    args = parse.parse_args()
+
+    main(args.url, args.filename)
